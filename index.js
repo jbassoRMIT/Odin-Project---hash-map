30922811af3c12f14b8020fa6758ec684ae49ea4
jbassoRMIT <s4101667@student.rmit.edu.au>
2025-12-02 08:16:51 +1100

    all functions implemented up to and including remove(key)

--- index.js
+++ index.js
@@ -1,10 +1,63 @@
 class HashMap{
-    constructor(loadFactor=0.75,capcity=16){
+    constructor(array={},loadFactor=0.75,capacity=16){
+        this.array=array;
         this.loadFactor=loadFactor;
-        this.capcity=capacity;
+        this.capacity=capacity;
+    }
+    hash(key){
+        let hashCode=0;
+
+        let prime=31;
+        for(let i=0;i<key.length;i++){
+            hashCode=prime*hashCode+key.charCodeAt(i);
+        }
+        return hashCode;
+    }
+
+    set(key,value){
+        this.array[key]=value;
+    }
+
+    get(key){
+        if(this.array[key]){
+            return this.array[key];
+        }
+        else{
+            return null;
+        }
+    }
+
+    has(key){
+        if(this.array[key]){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    remove(key){
+        //use delete method;
+        if(this.array[key]){
+            delete this.array[key];
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
 }
 
-const word="gdaymate";
-console.log(word.charCodeAt(2));
+const test=new HashMap();
+test.set('apple', 'red')
+test.set('banana', 'yellow')
+test.set('carrot', 'orange')
+test.set('banana', 'green')
+console.log(test.array);
+console.log(test.get("banana"));
+console.log(test.get("toy"));
+console.log(test.has("banana"));
+console.log(test.has("toy"))
+console.log(test.remove("banana"));
+console.log(test.array);
