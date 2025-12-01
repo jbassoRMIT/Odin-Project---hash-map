14bd776aeaa19274e0d6a51e7e9e65ac86a00bfb
jbassoRMIT <s4101667@student.rmit.edu.au>
2025-12-01 15:27:07 +1100

    index.js file setup and constructor for hashmap class established

---
+++ index.js
@@ -0,0 +1,10 @@
+class HashMap{
+    constructor(loadFactor=0.75,capcity=16){
+        this.loadFactor=loadFactor;
+        this.capcity=capacity;
+    }
+
+}
+
+const word="gdaymate";
+console.log(word.charCodeAt(2));
